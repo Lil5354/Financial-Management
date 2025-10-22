26cff72dad505ca7d7d8203d5644653d5c9dccc9
Lil5354 <dttthao.5354@gmail.com>
2025-10-22 10:25:49 +0700

    Initial commit

--- app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_example_expensetracker_ExpenseTrackerApplication.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.example.expensetracker.ExpenseTrackerApplication"
-)
-public final class _com_example_expensetracker_ExpenseTrackerApplication {
-}
